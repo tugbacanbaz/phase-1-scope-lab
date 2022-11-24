a101f627c04148c2724e2e99680a5b4d0c698151
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-11-24 23:18:33 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName="bob";
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer='John';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Adam';
+}
+//console.log(changeLeastFavoriteCustomer);
